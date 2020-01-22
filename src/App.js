9b9103bda13bbd17d0690a9c56c20e8911425d9c
georgelau <laugeorge@gmail.com>
2020-01-22 14:24:39 -0500

    Created Sign In and Sign Up page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React from 'react'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
+import ProjectDetails from './components/projects/ProjectDetails'
+import SignIn from './components/auth/SignIn'
+import SignUp from './components/auth/SignUp'
+
 
 function App() {
   return (
@@ -10,7 +14,12 @@ function App() {
         <Navbar />
         <Switch>
           {/* When user goes to /, load the Dashboard component*/}
-          <Route path = '/' component = { Dashboard } />
+          <Route exact path = '/' component = { Dashboard } />
+          <Route path = '/project/:id' component = { ProjectDetails } />
+          <Route path = '/signin' component = { SignIn } />
+          <Route path = '/signup' component = { SignUp } />
+
+
         </Switch>
       </div>
     </BrowserRouter>
