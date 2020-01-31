f1c8b7720971accf70f63adc36ecc0b4911ae489
georgelau <laugeorge@gmail.com>
2020-01-31 03:07:09 -0500

    update projectdetail projectsummary with momentjs to properly reflect dates

--- src/store/actions/projectAction.js
+++ src/store/actions/projectAction.js
@@ -3,11 +3,14 @@ export const createProject = (project) => {
     return (dispatch, getState, { getFirebase, getFirestore }) => {
         // make async call to database
         const firestore = getFirestore();
+        const profile = getState().firebase.profile;
+        const authorId = getState().firebase.auth.uid;
+
         firestore.collection('projects').add({
             ...project,
-            authorFirstName: 'George',
-            authorLastName: 'Lau',
-            authorID: 12345,
+            authorFirstName: profile.firstName,
+            authorLastName: profile.lastName,
+            authorID: authorId,
             createdAt: new Date()
         })
         .then(() => { dispatch({type: 'CREATE_PROJECT', project});
