001eeda7ebb092deed9daa060c81c72da08540ad
georgelau <laugeorge@gmail.com>
2020-03-11 04:33:22 -0400

    update README and firebase deploy

---
+++ (public) dist/precache-manifest.6e61d355a7aca1e1e7e0ceb7ab23b6f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d66850179851c1bbdb93d859eea46e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ceff99f934c1c6dc6cc0",
+    "url": "/static/css/main.62581cd7.chunk.css"
+  },
+  {
+    "revision": "4c2993341b388a728b79",
+    "url": "/static/js/2.3409706d.chunk.js"
+  },
+  {
+    "revision": "3160bf7c029258aba92ee24744af9381",
+    "url": "/static/js/2.3409706d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ceff99f934c1c6dc6cc0",
+    "url": "/static/js/main.05daa4dd.chunk.js"
+  },
+  {
+    "revision": "c80bae48cdf384b85e9c",
+    "url": "/static/js/runtime-main.7551f220.js"
+  }
+]);
